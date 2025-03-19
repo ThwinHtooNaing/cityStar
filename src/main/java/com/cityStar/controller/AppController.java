3510b35a58bfd615c3aef6d91036070727d7bf6f
Noah Thwin <sthit312@gmail.com>
2025-03-19 10:22:12 +0630

    add login and registration endpoints with corresponding view

--- src/main/java/com/cityStar/controller/AppController.java
+++ src/main/java/com/cityStar/controller/AppController.java
@@ -3,13 +3,17 @@ package com.cityStar.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/com")
 @Controller
 public class AppController {
 
     @RequestMapping("/login")
     public String LogIn() {
-        return "home";
+        return "login";
+    }
+
+    @RequestMapping("/register")
+    public String register() {
+        return "register";
     }
     
 }
